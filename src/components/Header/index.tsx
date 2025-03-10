627ba8dac8468f76360ee6243ba17a6cfa36776b
sollyigor <igor.solly@bazardoconsorcio.com.br>
2025-03-10 20:16:04 -0300

    Criação das Rotas / e Cart com React Router Dom

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,19 +2,24 @@ import coffeeDelivery from "../../assets/coffedelivery-logo.svg";
 import { CartButton } from "../CartButton";
 import styles from "./index.module.css";
 import { MapPin } from "@phosphor-icons/react";
+import { NavLink } from "react-router-dom";
 
 export function Header() {
   return (
     <div className="container">
       <header className={`${styles.header} flexContainer`}>
         <div>
-          <img src={coffeeDelivery} alt="" />
+          <NavLink to="/">
+            <img src={coffeeDelivery} alt="" />
+          </NavLink>
         </div>
         <div className={styles.actions}>
           <span className={styles.location}>
             <MapPin weight="fill" color="var(--purple)" /> Porto Alegre, RS
           </span>
-          <CartButton variant="header" />
+          <NavLink to="/cart">
+            <CartButton variant="header" />
+          </NavLink>
         </div>
       </header>
     </div>
